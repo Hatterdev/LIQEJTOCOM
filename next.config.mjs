fa031e31de3ece1b28d62febc6cc969270f6c17c
Hatterdev <120704180+Hatterdev@users.noreply.github.com>
2025-07-07 20:18:25 -0300

    Configura next-transpile-modules para transpilar @coinbase/wallet-sdk

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+import withTM from 'next-transpile-modules';
 
-export default nextConfig;
+// Coloque o nome exato do pacote que precisa ser transpileado
+const withTMConfig = withTM(['@coinbase/wallet-sdk']);
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  // outras configs que quiser adicionar
+};
+
+export default withTMConfig(nextConfig);
